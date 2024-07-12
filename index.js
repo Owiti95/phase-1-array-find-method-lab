1f7d286a258e9857bff320748984c547c8a8b3c6
hannington owiti <hanningtonowiti@gmail.com>
2024-07-12 14:23:22 +0300

    superbowl

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //superbowl data
+  ]
+
+function superbowlWin(record){
+    let win = record.find(function(superbowl) {
+        return superbowl.result === "W";
+    })
+    if(win){
+        return win.year;
+    }else{
+        return undefined;
+    }
+}
